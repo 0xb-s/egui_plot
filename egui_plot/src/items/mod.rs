fd2a17cb82381410e04d7a9a9fab0b29c2dba781
0xb-s <145866191+0xb-s@users.noreply.github.com>
2025-09-23 04:08:18 -0700

    Update mod.rs

--- egui_plot/src/items/mod.rs
+++ egui_plot/src/items/mod.rs
@@ -15,7 +15,9 @@ use emath::Float as _;
 use rect_elem::{RectElement, highlighted_color};
 
 use super::{Cursor, LabelFormatter, PlotBounds, PlotTransform};
-
+pub use crate::items::tooltip_band::BandTooltipOptions;
+pub use crate::items::tooltip_band::HitRow;
+pub use crate::items::tooltip_band::PinnedRow;
 pub use band::Band;
 pub use bar::Bar;
 pub use box_elem::{BoxElem, BoxSpread};
@@ -28,6 +30,7 @@ mod bar;
 mod box_elem;
 mod rect_elem;
 mod values;
+mod tooltip_band;
 
 const DEFAULT_FILL_ALPHA: f32 = 0.05;
 
